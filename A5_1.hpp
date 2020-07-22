938654c6712f4eb39915e3563a80916cfe37df5b
peterhao89 <haoyonglin@yeah.net>
2020-07-22 17:12:44 +0800

    ready to merge

--- A5_1.hpp
+++ A5_1.hpp
@@ -5,6 +5,7 @@
 #include <immintrin.h>
 #include<zmmintrin.h>
 #include<fstream>
+#include<iostream>
 using namespace std;
 #define LROT32(x,s)  (((x)<<s)|((x)>>(32-s)))
 #define RROT32(x,s)  (((x)>>s)|((x)<<(32-s)))
@@ -24,22 +25,46 @@ using namespace std;
 
 typedef uint64_t u64;
 
+u64 rand_64()//产生64位随机数
+{
+	static u64 Z[2] = { 0x375201345e7fa379, 0xcde9fe134e8af6b1 ^ (u64(rand()) << 32) ^ rand() };//(rand() << 24) + (rand() << 12) + rand() 0xcde9fb8a
+	u64 temp = Z[1] ^ (Z[1] << 63) ^ (Z[0] >> 1);
+	Z[0] = Z[1];
+	Z[1] = temp;
+	return Z[0];
+}
+
+void setBitVal(u64& reg, int bitNo, u64 val) {
+	u64 msk = 1;
+	msk <<= bitNo;
+	if (val != 0) {
+		reg |= msk;
+	}
+	else {
+		msk = ~msk;
+		reg &= msk;
+	}
+}
 class A5_1_S100 {
 public:
 
 	A5_1_S100(uint64_t state=0) {
+		update(state);
+	}
+
+	void update(u64 state ) {
 		R1 = state & R1MASK;
 		R2 = (state >> 19) & R2MASK;
 		R3 = (state >> (41)) & R3MASK;
 	}
-	
+	u64 getWholeState() {
+		return (R1 | (R2 << 19) | (R3 << 41));
+	}
 
 	u64 output() {
 		clockStopGo();
 		return (bit(R1,18) ^ bit(R2,21)^bit(R3,22));
 	}
-
-
 
 private:
 	u64 R1,R2,R3;
@@ -62,30 +87,56 @@ private:
 	void updateR1() {
 		u64 opt = bit(R1, 18) ^ bit(R1, 17) ^ bit(R1, 16) ^ bit(R1, 13);
 		R1 <<= 1;
+		R1 &= R1MASK;
 		setBitVal(R1, 0, opt);
 	}
 
 	void updateR2() {
 		u64 opt = bit(R2, 21) ^ bit(R2, 20);
 		R2 <<= 1;
+		R2 &= R2MASK;
 		setBitVal(R2, 0, opt);
 	}
 
 	void updateR3() {
 		u64 opt = bit(R3, 22) ^ bit(R3, 21) ^ bit(R3, 20) ^ bit(R3, 7);
 		R3 <<= 1;
+		R3 &= R3MASK;
 		setBitVal(R3, 0, opt);
 	}
 
-	void setBitVal(u64& reg, int bitNo, u64 val) {
-		u64 msk = 1;
-		msk <<= bitNo;
-		if (val == 0) {
-			reg |= msk;
-		}
-		else {
-			msk= ~msk;
-			reg &= msk;
-		}
+};
+
+
+class A5_1_S100ANF {
+public:
+
+	A5_1_S100ANF(uint64_t state = 0) {
+		update(state);
 	}
+	void update(u64 state ) {
+		R1 = state & R1MASK;
+		R2 = (state >> 19) & R2MASK;
+		R3 = (state >> (41)) & R3MASK;
+	}
+	u64 output(int offset) {
+		u64 x[9] = { bit(R1,18 + offset),
+			bit(R2,21+ offset),
+			bit(R3,22 + offset),
+			bit(R1,17 + offset),
+			bit(R2,20 + offset),
+			bit(R3,21 + offset),
+			bit(R1,8 + offset),
+			bit(R2,10 + offset),
+			bit(R3,10 + offset) };
+		u64 res = x[3] ^ x[4] ^ x[5] ^ (x[0] & x[6]) ^ (x[3] & x[6]) ^ (x[1] & x[7]) ^ (x[4] & x[7]) ^ (x[2] & x[8]) ^ (x[5] & x[8]) ^ (x[0] & x[6] & x[7])
+			^ (x[1] & x[6] & x[7]) ^ (x[2] & x[6] & x[7]) ^ (x[3] & x[6] & x[7]) ^ (x[4] & x[6] & x[7]) ^ (x[5] & x[6] & x[7]) ^ (x[0] & x[6] & x[8])
+			^ (x[1] & x[6] & x[8]) ^ (x[2] & x[6] & x[8]) ^ (x[3] & x[6] & x[8]) ^ (x[4] & x[6] & x[8]) ^ (x[5] & x[6] & x[8]) ^ (x[0] & x[7] & x[8])
+			^ (x[1] & x[7] & x[8]) ^ (x[2] & x[7] & x[8]) ^ (x[3] & x[7] & x[8]) ^ (x[4] & x[7] & x[8]) ^ (x[5] & x[7] & x[8]);
+		return res;
+	}
+
+
+private:
+	u64 R1, R2, R3;
 };
---
+++ mainA51.cpp
@@ -0,0 +1,57 @@
+#include"A5_1.hpp"
+
+
+int main() {
+	u64 count = 0;
+	int prefixLength = 3;
+	vector<int> relatedBitsOf1Output = { 18, 19 + 21, 41 + 22, 17,19 + 20, 41 + 21, 8, 19 + 10, 41 + 10 };
+	set<int> relatedBitsOf5Outputs;
+	for (int offset = 0; offset < 3; ++offset) {
+		for (int i = 0; i < relatedBitsOf1Output.size(); ++i) {
+			relatedBitsOf5Outputs.insert(relatedBitsOf1Output[i]+offset);
+		}
+	}
+	vector<int> involvedBits;
+	for (set<int>::iterator ite = relatedBitsOf5Outputs.begin(); ite != relatedBitsOf5Outputs.end(); ++ite) {
+		involvedBits.push_back(*ite);
+	}
+	int involvedBitNumber = involvedBits.size();
+	u64 total = 1;
+	total <<= involvedBitNumber;
+	set<u64> Lz0_z4;
+
+	A5_1_S100 runner(0);
+	u64 iniState = 0;
+	bool satisfyPattern = true;
+	u64 pattern = 0x1f;
+	vector<u64> patternBits;
+	for (int i = 0; i < prefixLength; ++i) {
+		patternBits.push_back(bit(pattern, i));
+	}
+	for (count = 0; count < total; ++count) {
+		iniState = 0;
+		satisfyPattern = true;
+		for (int loop = 0; loop < involvedBits.size(); ++loop) {
+			setBitVal(iniState, involvedBits[loop], bit(count, loop));
+		}
+		runner.update(iniState);
+		for (int i = 0; i < prefixLength; ++i) {
+			if (runner.output() != patternBits[i]) {
+				satisfyPattern = false;
+				break;
+			}
+		}
+		if (satisfyPattern) {
+			Lz0_z4.insert(iniState);
+		}
+	}
+
+	cout << "The merged list L has #L=" << Lz0_z4.size() << endl;
+
+
+
+	getchar();
+	return 0;
+
+
+}
